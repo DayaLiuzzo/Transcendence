17931b0dcef54ade89e90b646efa7d83f39fcacf
rinmadv <marine.mardavid@gmail.com>
2024-11-22 14:57:23 +0100

    basic spa works

--- srcs/services/frontend/spa/js/main.js
+++ srcs/services/frontend/spa/js/main.js
@@ -1,15 +1,21 @@
 // Fonction pour charger les modules de vues (home, jeu, 404)
 function loadView(view) {
     const app = document.getElementById('app');
-  
     // Vérification des vues disponibles et importation de la vue correspondante
+    // Step 1: Access the current URL
+
+    // Remove hash from the url if /home#rerer -> /home
+    const currentURL = window.location.pathname
+    // if (!currentURL.endsWith('/')) currentURL += '/';
+    window.history.pushState("", document.title, currentURL.split('#')[0]);
+    
     switch (view) {
-      case 'home':
+      case '/home':
         import('./home.js').then(module => {
           app.innerHTML = module.default();
         });
         break;
-      case 'jeu':
+      case '/jeu':
         import('./jeu.js').then(module => {
           app.innerHTML = module.default();
         });
@@ -26,21 +32,23 @@ function loadView(view) {
   document.querySelectorAll('a').forEach(link => {
     link.addEventListener('click', function (e) {
       e.preventDefault();
-      const view = link.getAttribute('href').replace('#', ''); // On prend la partie après le hash
-      window.location.hash = view;  // Change l'URL dans la barre d'adresse
+      const view = link.getAttribute('href'); // On prend la partie après le hash
+      window.location.pathname = view;  // Change l'URL dans la barre d'adresse
       loadView(view);  // Charge la vue correspondante
     });
   });
   
   // Gérer les changements dans l'URL via l'API hashchange
   window.addEventListener('hashchange', () => {
-    const view = window.location.hash.replace('#', '') || 'home';  // Par défaut, on charge "home"
+    // const view = window.location.pathname.replace('#', '') || '404';  // Par défaut, on charge "home"
+    const view = window.location.pathname;  // Par défaut, on charge "home"
     loadView(view);
   });
   
   // Charger la vue par défaut au chargement de la page
   window.addEventListener('DOMContentLoaded', () => {
-    const view = window.location.hash.replace('#', '') || 'home';  // Vérifie l'URL
+    const view = window.location.pathname;  // Vérifie l'URL
+    console.log('window location object:', window.location)
     loadView(view);
   });
   
