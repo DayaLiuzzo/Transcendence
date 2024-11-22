aa2e1b71f48431eae13f381caf45101c1810a98c
rinmadv <marine.mardavid@gmail.com>
2024-11-22 16:43:06 +0100

    js pages for spa in progress

---
+++ srcs/services/frontend/spa/js/about.js
@@ -0,0 +1,7 @@
+export default function() {
+    return `
+      <h1>About les stawrs</h1>
+      <p>C'est nous les super stars.</p>
+    `;
+  }
+  
--- srcs/services/frontend/spa/js/play.js
+++ srcs/services/frontend/spa/js/play.js
@@ -3,6 +3,9 @@ export default function() {
       <h1>Bienvenue dans le jeu</h1>
       <p>Cette section peut contenir un jeu ou une interface de jeu.</p>
       <button onclick="alert('Jeu lancé !')">Démarrer le jeu</button>
+      <button onclick>Play alone</button>
+      <button onclick>Play in tournament</button>
+      <button onclick>Custom game</button>
     `;
   }
   
