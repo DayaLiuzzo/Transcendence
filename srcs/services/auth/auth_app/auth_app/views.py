8201311d1f2cbef0ca6b590235a6c6827bac74c6
Daya Liuzzo <daya.liuzzo@gmail.com>
2024-11-22 19:27:13 +0100

    ADD Token generation

--- srcs/services/auth/auth_app/auth_app/views.py
+++ srcs/services/auth/auth_app/auth_app/views.py
@@ -1,4 +1,8 @@
 from django.http import JsonResponse
 
+def api_auth_view(request):
+    return JsonResponse({"message": "Auth API is running"})
+
+
 def auth_view(request):
     return JsonResponse({"message": "Auth service is running"})
