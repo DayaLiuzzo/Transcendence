ed4300764ed6089dfce2978e4e1146ab0f847064
Daya Liuzzo <daya.liuzzo@gmail.com>
2024-11-25 17:25:21 +0100

    ADD JWT/API/AUTH creating api_views for token authentication, not embedded yet

--- srcs/services/auth/auth_app/auth_app/urls.py
+++ srcs/services/auth/auth_app/auth_app/urls.py
@@ -1,14 +1,9 @@
 
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework import routers
 
 urlpatterns = [
-    path('api/auth/admin/', admin.site.urls),
-    path('auth/', views.auth_view, name='auth'),
-    path('api/auth/', include('rest_framework.urls')),
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path('login', views.login),
+    re_path('signup', views.signup),
+    re_path('test_token', views.test_token),
 ]
