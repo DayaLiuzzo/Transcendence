23628492cdff04cb4082640ea4e647e31ee4239b
rinmadv <marine.mardavid@gmail.com>
2024-11-28 17:16:45 +0100

    spa fixed

--- srcs/services/frontend/spa/js/main.js
+++ srcs/services/frontend/spa/js/main.js
@@ -1,114 +1,78 @@
 // Fonction pour charger les modules de vues (home, jeu, 404)
-function loadView(view) {
+function loadView(view)
+{
     const app = document.getElementById('app');
-    // Vérification des vues disponibles et importation de la vue correspondante
-    // Step 1: Access the current URL
 
-    // Remove hash from the url if /home#rerer -> /home
-    const currentURL = window.location.pathname
-    // if (!currentURL.endsWith('/')) currentURL += '/';
+    const currentURL = view
     window.history.pushState("", document.title, currentURL.split('#')[0]);
     
     switch (view) {
       case '/home':
-        import('./home.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./home.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/':
-        import('./home.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./home.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/play':
-        import('./play.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./play.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/about':
-        import('./about.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./about.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/log-in':
-        import('./log-in.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./log-in.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/sign-up':
-        import('./sign-up.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./sign-up.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/delete-user':
-        import('./delete-user.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./delete-user.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/edit-profile':
-        import('./edit-profile.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./edit-profile.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/game-custom':
-        import('./game-custom.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./game-custom.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/play-tournament':
-        import('./play-tournament.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./play-tournament.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/game':
-        import('./game.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./game.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/play-alone':
-        import('./play-alone.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./play-alone.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/game-ai':
-        import('./game-ai.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./game-ai.js').then(module => { app.innerHTML = module.default();});
         break;
       case '/game-multi':
-        import('./game-multi.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./game-multi.js').then(module => { app.innerHTML = module.default();});
         break;
       default:
-        import('./404.js').then(module => {
-          app.innerHTML = module.default();
-        });
+        import('./404.js').then(module => { app.innerHTML = module.default();});
         break;
     }
   }
   
-  // Gérer les liens dans la navigation
-  document.querySelectorAll('a').forEach(link => {
+document.querySelectorAll('a').forEach(link =>
+{
     link.addEventListener('click', function (e) {
       e.preventDefault();
-      const view = link.getAttribute('href'); // On prend la partie après le hash
-      window.location.pathname = view;  // Change l'URL dans la barre d'adresse
-      loadView(view);  // Charge la vue correspondante
+      const view = link.getAttribute('href');
+      loadView(view);
     });
-  });
-  
-  // Gérer les changements dans l'URL via l'API hashchange
-  window.addEventListener('hashchange', () => {
-    // const view = window.location.pathname.replace('#', '') || '404';  // Par défaut, on charge "home"
-    const view = window.location.pathname;  // Par défaut, on charge "home"
-    loadView(view);
-  });
-  
-  // Charger la vue par défaut au chargement de la page
-  window.addEventListener('DOMContentLoaded', () => {
-    const view = window.location.pathname;  // Vérifie l'URL
-    console.log('window location object:', window.location)
-    loadView(view);
-  });
+});
+
+window.addEventListener('DOMContentLoaded', () =>
+{
+  const view = window.location.pathname;
+  loadView(view);
+});
+
+window.addEventListener('popstate', () =>
+{
+  const view = window.location.pathname;
+  loadView(view);  
+})
   
