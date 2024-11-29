c21d45e7cef2f63f91012a009365dd74739ece81
rinmadv <marine.mardavid@gmail.com>
2024-11-29 17:20:45 +0100

    oups jai oublie la piece jointe

--- srcs/services/frontend/spa/js/404.js
+++ srcs/services/frontend/spa/js/404.js
@@ -2,6 +2,7 @@ export default function() {
     return `
       <h1>404 - Page non trouvée</h1>
       <p>La page que vous recherchez n'existe pas bande de clochards.</p>
+      <img src="../media/meuriah.gif" alt="Profile picture" class="profile-img">
     `;
   }
   
