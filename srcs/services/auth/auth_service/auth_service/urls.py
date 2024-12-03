73b9152cc2235bb1099e73d0f3f493f29148677f
rinmadv <marine.mardavid@gmail.com>
2024-12-03 16:47:45 +0100

    working on api gateway

--- srcs/services/auth/auth_service/auth_service/urls.py
+++ srcs/services/auth/auth_service/auth_service/urls.py
@@ -1,9 +1,10 @@
 
-from django.urls import re_path, include
+from django.urls import path, re_path, include
 from auth_app import views
 
 urlpatterns = [
-    path('/api/auth/', include([
+    path('api/auth/', views.auth_root),
+    path('api/auth/', include([
         re_path('login', views.login),
         re_path('signup', views.signup),
         re_path('test_token', views.test_token),
