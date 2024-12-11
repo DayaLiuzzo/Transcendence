dd74d9ced566c372f0889c136f00059eedd08561
Daya Liuzzo <daya.liuzzo@gmail.com>
2024-12-11 19:34:05 +0100

    ADD create UserProfile/users \ PATCH NGINX desactivation API gateway, upstream auth

--- srcs/services/users/users_service/users_app/views.py
+++ srcs/services/users/users_service/users_app/views.py
@@ -2,14 +2,29 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
-from .serializers import UserSerializer
+from .serializers import UserProfileSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status 
+from rest_framework import status
+from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from .models import UserProfile
 
+from django.db import IntegrityError
 
-
+class CreateUserProfileView(APIView):
+    def post(self, request):
+        serializer = UserProfileSerializer(data=request.data)
+        try:
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except IntegrityError:
+            return Response({'error': 'Database integrity error occurred (pseudo may already exist).'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 # def api_auth_view(request):
 #     return JsonResponse({"message": "Auth API is running"})
 
