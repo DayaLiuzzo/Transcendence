a11032d623c0fcfcab85cf83a655e922385e4e98
Daya Liuzzo <daya.liuzzo@gmail.com>
2024-12-11 17:35:43 +0100

    ADD signup : Creation of User in AUTH/DB_AUTH

--- srcs/services/api_gateway/api_gateway_service/api_gateway_service/settings.py
+++ srcs/services/api_gateway/api_gateway_service/api_gateway_service/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -129,9 +129,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "https://localhost:4430",  # SPA frontend URL
 ]
-CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_SECURE = False
+
+
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:4430",
+    "https://localhost:4430",
 ]
+
+CSRF_COOKIE_DOMAIN = "localhost"
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
--- srcs/services/auth/auth_service/auth_app/serializers.py
+++ srcs/services/auth/auth_service/auth_app/serializers.py
@@ -5,4 +5,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
         fields = ['id', 'username', 'password', 'email']
-
+    def create(self, validated_data):
+        # Hash the password and create the user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
