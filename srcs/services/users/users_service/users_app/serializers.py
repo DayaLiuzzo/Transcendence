eece82abe4f7d28b08a052b898b9cc6919c3bb21
Daya Liuzzo <daya.liuzzo@gmail.com>
2024-12-12 16:49:48 +0100

    ADD : USERS/ endpoint to create UserProfile Upon signup on AUTH \\ ADD : AUTH / requestcustoms to send api request to other services

--- srcs/services/users/users_service/users_app/serializers.py
+++ srcs/services/users/users_service/users_app/serializers.py
@@ -5,27 +5,8 @@ from rest_framework import serializers
 from .models import UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    biography = serializers.CharField(default="Default biography", required=False)
     class Meta:
         model = UserProfile
-        fields = ['pseudo', 'biography']
-    
-    def validate(self, data):
-        """Check for extra fields in the request."""
-        allowed_fields = set(self.fields.keys())
-        request_fields = set(self.initial_data.keys())  # `initial_data` contains the raw input
-        extra_fields = request_fields - allowed_fields
-        
-        if extra_fields:
-            raise serializers.ValidationError(
-                {"detail": f"Extra fields not allowed: {', '.join(extra_fields)}"}
-            )
-        return data
+        fields = ['username', 'biography']
 
-    def validate_pseudo(self, value):
-        """Check that the pseudo is unique."""
-        if UserProfile.objects.filter(pseudo=value).exists():
-            raise serializers.ValidationError("This pseudo already exists.")
-        return value
-    
-    def create(self, validated_data):
-        return UserProfile.objects.create(**validated_data)
