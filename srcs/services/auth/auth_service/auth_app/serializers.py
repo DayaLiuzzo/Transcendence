9720fdbc7643158ec6fe9fc9990f84d32bfc9c76
Daya Liuzzo <daya.liuzzo@gmail.com>
2024-12-12 15:22:42 +0100

    MOD AUTH -> modified sign up view for better encapsulation

--- srcs/services/auth/auth_service/auth_app/serializers.py
+++ srcs/services/auth/auth_service/auth_app/serializers.py
@@ -1,15 +1,30 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
-        fields = ['id', 'username', 'password', 'email']
+        fields = ['username', 'password', 'email']
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("This username already exists.")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email already exists.")
+        return value
+    
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+        return value
+        
     def create(self, validated_data):
         # Hash the password and create the user
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
+        validated_data['password'] = make_password(validated_data['password'])
+        user = User.objects.create(**validated_data)
+
         return user
