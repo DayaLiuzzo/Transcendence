b4e8cdf95a3660e7d0827ddb05027c0d12dfc029
Daya Liuzzo <dliuzzo@made-f0Ar1s4.clusters.42paris.fr>
2024-12-18 16:56:57 +0100

    feat(auth): add RS256 signing algorithm for SJWT

--- srcs/services/auth/auth_service/auth_service/settings.py
+++ srcs/services/auth/auth_service/auth_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'auth_app',
+    'rest_framework_simplejwt.token_blacklist'
 ]
 
 REST_FRAMEWORK = {
@@ -112,6 +114,35 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+def get_sjwt_key(key):
+    with open(key, "r") as file:
+        return file.read()
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=90),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": False,
+    "ALGORITHM": "RS256",
+    "SIGNING_KEY": get_sjwt_key("/keys/sjwt_private.pem"),
+    "VERIFYING_KEY": get_sjwt_key("/keys/sjwt_public.pem"),
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JSON_ENCODER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+    "JTI_CLAIM": "jti",
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.CustomTokenObtainPairSerializer",
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
