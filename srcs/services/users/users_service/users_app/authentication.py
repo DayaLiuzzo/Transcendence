88298e4271a0e954d0b86cd027a1d4a5e138e7a2
Daya Liuzzo <dliuzzo@made-f0Ar5s6.clusters.42paris.fr>
2024-12-24 16:05:55 +0100

    debug(users): mod logs

--- srcs/services/users/users_service/users_app/authentication.py
+++ srcs/services/users/users_service/users_app/authentication.py
@@ -3,15 +3,28 @@ from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework import exceptions
 from .models import UserProfile
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    # filename="basic.log",
+    )
+
+# Crée un logger spécifique au module courant
+logger = logging.getLogger(__name__)
+
+
 
 class CustomJWTAuth(BaseAuthentication):
-    def __init(self):
+    def __init__(self): 
         from users_service.settings import SIMPLE_JWT
         self.algorithm = SIMPLE_JWT["ALGORITHM"]
         self.verifying_key = SIMPLE_JWT["VERIFYING_KEY"]
         self.auth_header_types = SIMPLE_JWT["AUTH_HEADER_TYPES"]
-
-    def auhtenticate(self, request):
+        
+    def authenticate(self, request):
         auth_header = request.headers.get("Authorization")
         if not auth_header:
             return None
@@ -39,14 +52,15 @@ class CustomJWTAuth(BaseAuthentication):
         return (user, payload)
     
     def get_user_from_payload(self, payload):
-
-        username = payload.get("username")  # Or `username` depending on your claims
+        username = payload.get("username")  # Extracting `username` from the token
         if not username:
-            raise AuthenticationFailed("Token payload is missing 'sub'.")
+            raise AuthenticationFailed("Token payload is missing 'username'.")
         
         try:
             user = UserProfile.objects.get(username=username)
+            return user
         except UserProfile.DoesNotExist:
+            logger.debug(f"User with username '{username}' does not exist.")  # Debug line
             raise AuthenticationFailed("User does not exist.")
         
         return user
