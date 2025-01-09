559fc45afe236db0f32b76145433e4a4a75fbb62
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-01-09 14:56:40 +0100

    chore(docker) add volumes for api_gateway

--- srcs/services/api_gateway/init/start.sh
+++ srcs/services/api_gateway/init/start.sh
@@ -1,7 +1,7 @@
 /bin/bash
 
 pip install --upgrade pip --no-input
-pip install -r ../requirements.txt --no-input
+pip install -r /init/requirements.txt --no-input
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
