476cd88e13f0caa1485f165f08d58954160cfb6b
Marine DAVID <madavid@paul-f4Br7s4.clusters.42paris.fr>
2025-01-16 16:33:30 +0100

    added foreign keys to link rooms attributes in user and users attributes in room + added some checks for room joining

--- srcs/services/rooms/rooms_service/rooms_app/serializers.py
+++ srcs/services/rooms/rooms_service/rooms_app/serializers.py
@@ -4,19 +4,24 @@ from .models import User
 from .models import Room
 
 class RoomSerializer(serializers.ModelSerializer):
+    player1_username = serializers.CharField(source='player1.username', read_only=True)  # Affiche le nom de player1
+    player2_username = serializers.CharField(source='player2.username', read_only=True)  # Affiche le nom de player2
+
     class Meta:
         model = Room
-        fields = ['room_id', 'players_count', 'status']
+        fields = ['room_id', 'player1_username', 'player2_username', 'players_count', 'status']
 
-    def get_is_full(self, obj):
+    @property
+    def is_full(self, obj):
         return obj.players_count >= 2
     
 class UserSerializer(serializers.ModelSerializer):
-    is_authenticated = serializers.SerializerMethodField() #pour dommer acces explicite dans le serializer
+    is_authenticated = serializers.SerializerMethodField()
+    room_id = serializers.CharField(source='room.room_id', read_only=True)
     
     class Meta:
         model = User
-        fields = ['username', 'isconnected', 'is_authenticated']
+        fields = ['username', 'isconnected', 'is_authenticated', 'room_id']
 
     def get_is_authenticated(self, obj):
         return obj.is_authenticated
