f03e2e9a281f9e2b4e8a65966dd9c8fa8ece0777
Marine DAVID <madavid@paul-f4Br7s4.clusters.42paris.fr>
2025-01-16 15:07:28 +0100

    fixed token problem with Daya, improved code by replacing function based views by class based views, implemented CRUD methods and tests

--- srcs/services/rooms/rooms_service/rooms_service/urls.py
+++ srcs/services/rooms/rooms_service/rooms_service/urls.py
@@ -7,8 +7,20 @@ from rooms_app import views
 urlpatterns = [
     path('api/rooms/', include([
         path('test/', views.rooms_service_running, name='rooms_home'),
-        path('join_or_create/', views.join_or_create_room, name='join_or_create_room'),
+        
+        path('create_room/', views.CreateRoomView.as_view(), name='create_room'),
+        path('join_room/', views.JoinRoomView.as_view(), name='join_room'),
+        path('list_all_rooms/', views.ListAllRoomsView.as_view(), name='list_room'),
+        path('list_available_rooms/', views.ListAvailableRoomsView.as_view(), name='list_room'),
+        path('list_locked_rooms/', views.ListLockedRoomsView.as_view(), name='list_room'),
+        path('count_all_rooms/', views.CountAllRoomsView.as_view(), name='count_all_rooms'),
+        path('count_available_rooms/', views.CountAvailableRoomsView.as_view(), name='count_available_rooms'),
+        path('count_locked_rooms/', views.CountLockedRoomsView.as_view(), name='count_locked_rooms'),
+        path('delete_room/<str:room_id>/', views.DeleteRoomView.as_view(), name='delete_room'),
+
         path('create/', views.CreateUserView.as_view(), name='create_user'),
-        path('', views.get_rooms, name='get_rooms'),
+        path('list/', views.ListUserView.as_view(), name='list_user'),
+        path('update/', views.UpdateUserView.as_view(), name='update_user'),
+        path('delete/', views.DeleteUserView.as_view(), name='delete_user'),
     ]))
 ]
