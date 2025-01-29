8bf96bb9b0dff6a7f172e84c3ecf392fdc805d4e
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-01-29 19:07:22 +0100

    debug(Auth) added unit tester for all AUTH views

--- srcs/services/auth/auth_service/auth_service/urls.py
+++ srcs/services/auth/auth_service/auth_service/urls.py
@@ -8,18 +8,5 @@ from auth_app.views import CustomTokenObtainPairView, TwoFactorSetupView, TwoFac
 
 urlpatterns = [
     # path('api/auth/', views.auth_root),
-    path('api/auth/', include([
-        path('refresh/', TokenRefreshView.as_view() , name='auth-refresh'),
-        path('logout/', TokenBlacklistView.as_view(), name='auth-logout'),
-        path('welcome/', views.welcome),
-        path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-        path('protected_user/', views.ProtectedUserView.as_view(), name='protected_user_view'),
-        path('signup/', views.SignUpView.as_view(), name='signup'),
-        path('delete/<str:username>/', views.DeleteUserView.as_view(), name='delete'),
-        path('service-token/', views.ServiceJWTObtainPair.as_view(), name='create-service-token'),
-        path('<str:username>/', views.RetrieveUserView.as_view(), name='user_auth'),
-        path('2fa/setup/', TwoFactorSetupView.as_view(), name='two_factor_setup'),
-        path('2fa/verify/', TwoFactorVerifyView.as_view(), name='two_factor_verify'),
-
-    ]))
+    path('api/auth/', include('auth_app.urls')),
 ]
--- srcs/services/avatar/avatar_service/avatar_app/serializers.py
+++ srcs/services/avatar/avatar_service/avatar_app/serializers.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
 from PIL import Image
-import io
 
-class UserAvatarSerializer(serializers.ModelSerializer):
+class UserAvatarSerializer(serializers.Serializer):
     avatar = serializers.ImageField(required=True)
     avatar_name = serializers.CharField(read_only=True)
+
     def validate_avatar(self, value):
         try:
             image = Image.open(value)
-            image.verify()
-        except (IOError, SyntaxError) as e:
+            image.verify()  # Verify integrity, but this closes the file
+            value.seek(0)   # Reset file pointer
+            image = Image.open(value)  # Reopen for format check
+        except (IOError, SyntaxError):
             raise serializers.ValidationError("The uploaded file is not a valid image.")
-        if image.format not in ['JPEG', 'PNG', 'JPG']
+
+        if image.format not in ['JPEG', 'PNG']:  # PIL returns "JPEG", not "JPG"
             raise serializers.ValidationError("The uploaded image has wrong format")
-        max_size = 5* 1024 * 1024
-        if image.size > max_size:
+
+        max_size = 5 * 1024 * 1024  # 5MB limit
+        if value.size > max_size:
             raise serializers.ValidationError("The image file size is too large")
+
         return value
