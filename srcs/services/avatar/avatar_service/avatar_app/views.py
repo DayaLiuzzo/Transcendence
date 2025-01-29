7012f85efc64d936bc83c3c0e41c4cee0060d20d
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-01-29 19:55:01 +0100

    feat(Avatar) Add Get Method for default avatar path

--- srcs/services/avatar/avatar_service/avatar_app/views.py
+++ srcs/services/avatar/avatar_service/avatar_app/views.py
@@ -10,17 +10,17 @@ from avatar_app.permissions import UserIsAuthenticated
 class AvatarView(APIView):
     permission_classes = [UserIsAuthenticated]
     def get(self, request, *args, **kwargs):
-        user = request.user
-        return Response({"message": f"Service {user.username}is Authenticated for GET"})
-    
-    def patch(self, request, *args, **kwargs):
-        user = request.user
-        return Response({"message": f"Service {user.username}is Authenticated for PATCH"})
+        links = ['/media/default_avatars/default_00.jpg',]
+        return Response(links, status=200)
     
     def post(self, request, *args, **kwargs):
         user = request.user
         return Response({"message": f"Service {user.username}is Authenticated for POST"})
 
+    def patch(self, request, *args, **kwargs):
+        user = request.user
+        return Response({"message": f"Service {user.username}is Authenticated for PATCH"})
+    
     
     def delete(self, request, *args, **kwargs):
         user = request.user
