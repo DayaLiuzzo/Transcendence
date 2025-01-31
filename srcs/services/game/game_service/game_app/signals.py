46c6bb3cade3350f801c0f61b6616d0cf1cb7394
Marine <madavid@student.42.fr>
2025-01-31 18:48:50 +0100

    fixed last issue by creating game and objects when called by create and join game endpoints + deleted signals stuffs (not useful anymore)

--- srcs/services/game/game_service/game_app/signals.py
+++ srcs/services/game/game_service/game_app/signals.py
@@ -8,32 +8,3 @@ import logging
 
 # Configure logging
 logger = logging.getLogger('game_app')
-
-@receiver(post_save, sender=Game)
-def create_game_objects(sender, instance, created, **kwargs):
-    if created:
-        print(f"Création du jeu {instance.room_id}...")
-
-        ball = Ball.objects.create(game=instance)
-        logger.debug(f"Balle créée : {ball}")
-
-        paddle_left = Paddle.objects.create(
-            game=instance,
-            player=instance.player1,
-            side='left',
-            x_position=-10.0
-        )
-
-        logger.debug(f"Paddle gauche créé : {paddle_left}")
-
-        paddle_right = Paddle.objects.create(
-            game=instance,
-            player=instance.player2,
-            side='right',
-            x_position=10.0
-        )
-        
-        logger.debug(f"Paddle droit créé : {paddle_right}")
-
-
-        instance.paddles.add(paddle_left, paddle_right)
--- srcs/services/game/game_service/game_app/views.py
+++ srcs/services/game/game_service/game_app/views.py
@@ -6,11 +6,13 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
-
+from .models import Game
 from .models import User
 from .models import Ball
 from .models import Paddle
+from .serializers import GameSerializer
 from .serializers import UserSerializer
 from .serializers import BallSerializer
 from .serializers import PaddleSerializer
@@ -102,9 +104,84 @@ class DeleteUserView(generics.DestroyAPIView):
 
 ################################################################
 #                                                              #
-#                       Game state views                       #
+#                          Game views                          #
 #                                                              #
 ################################################################
+
+# *************************** CREATE ************************* #
+
+#add les permissions : microservice room
+#ajouter le lien avec le service room
+class CreateGame(APIView):
+    def post(self, request, room_id, *args, **kwargs):
+        if Game.objects.filter(room_id=room_id).exists():
+            return Response({'error': f'La room avec l\'ID {room_id} existe déjà.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        game = Game(room_id=room_id)
+        game.save(force_insert=True)
+
+       # Créer la ball associée à la game
+        ball = Ball(game=game)
+        ball.save(force_insert=True)
+
+        # Créer les paddles pour chaque joueur (sans utilisateur assigné pour l'instant)
+        left_paddle = Paddle(game=game, side='left', x_position=-2.0)
+        right_paddle = Paddle(game=game, side='right', x_position=2.0)
+        left_paddle.save()
+        right_paddle.save()
+
+        # Associer la ball et les paddles à la game
+        game.ball = ball
+        game.paddles.set([left_paddle, right_paddle])
+        game.save()
+
+        game_serializer = GameSerializer(game)
+
+        return Response({'message': f'La room {room_id} a été créée avec succès.', 'game': game_serializer.data}, status=status.HTTP_201_CREATED)
+
+#add les permissions : users qui joinent
+class JoinGame(APIView):
+    def post(self, request, room_id, *args, **kwargs):
+        game = get_object_or_404(Game, room_id=room_id)
+
+
+        # Vérifier s'il y a une place pour l'utilisateur
+        user = request.user
+
+        if game.player1 is None:
+            # Si la place de player1 est vide, l'assigner à player1
+            game.player1 = user
+            left_paddle = game.paddles.filter(side='left').first()
+            left_paddle.player = user
+            left_paddle.save()
+            game.save()
+            return Response({
+                'message': f'{user.username} a rejoint la room {room_id} en tant que joueur 1.'
+            }, status=status.HTTP_200_OK)
+
+        elif game.player2 is None:
+            # Si la place de player2 est vide, l'assigner à player2
+            game.player2 = user
+            right_paddle = game.paddles.filter(side='right').first()
+            right_paddle.player = user
+            right_paddle.save()
+            game.save()
+            return Response({
+                'message': f'{user.username} a rejoint la room {room_id} en tant que joueur 2.'
+            }, status=status.HTTP_200_OK)
+
+        else:
+            # Si les deux places sont occupées
+            return Response({
+                'error': 'La room est déjà pleine.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+
+# *************************** READ *************************** #
+
+class GameListAPIView(generics.ListAPIView):
+    queryset = Game.objects.all()  # Récupère toutes les parties
+    serializer_class = GameSerializer  # Utilise le serializer des parties
 
 #a proteger
 class GameStateAPIView(APIView):
@@ -119,3 +196,22 @@ class GameStateAPIView(APIView):
             'ball': ball_serializer.data,
             'paddles': paddles_serializer.data
         })
+    
+# ************************** DELETE ************************** #
+
+class DeleteGame(APIView):
+    def delete(self, request, room_id, *args, **kwargs):
+        game = get_object_or_404(Game, room_id=room_id)
+
+        paddles = game.paddles.all()
+        for paddle in paddles:
+            paddle.delete()
+
+        if game.ball:
+            game.ball.delete()
+
+        game.delete()
+
+        return Response({
+            'message': f'La partie avec l\'ID {room_id} a été supprimée avec succès.'
+        }, status=status.HTTP_200_OK)
