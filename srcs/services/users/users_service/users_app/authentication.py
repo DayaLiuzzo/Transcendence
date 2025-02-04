9c402ce30727f46ae10cc1e7603b7095ee62dea2
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-02-04 10:50:21 +0100

    feat(backends) add get_service_token

--- srcs/services/users/users_service/users_app/authentication.py
+++ srcs/services/users/users_service/users_app/authentication.py
@@ -18,80 +18,44 @@ logger = logging.getLogger(__name__)
 
 
 class CustomJWTAuth(BaseAuthentication):
-    # def authenticate(self, request):
-    #     auth_header = request.headers.get('Authorization')
-    #     if not auth_header:
-    #         return None
-    #     token_type, token = self._extract_token(auth_header)
-    #     payload = self._decode_token(token)
-    #     user = self._get_user_from_payload(payload)
-    #     if user == None:
-    #         return (None, None)
-    #     return (user, token)
-    
-    # def _extract_token(self, auth_header):
-    #     try:
-    #         token_type, token = auth_header.split()
-    #         if token_type != 'Bearer':
-    #             raise AuthenticationFailed("Invalid token type. Expected 'Bearer'.")
-    #         return token_type, token
-    #     except ValueError:
-    #         raise AuthenticationFailed("Invalid Authorization header format. Expected 'Bearer <token>'.")
-    
-    # def _decode_token(self, token):
-    #     try:
-    #         return jwt.decode(
-    #             token,
-    #             settings.SIMPLE_JWT['VERIFYING_KEY'],
-    #             algorithms=settings.SIMPLE_JWT['ALGORITHM']
-    #         )
-    #     except jwt.ExpiredSignatureError:
-    #         raise AuthenticationFailed("The token has expired.")
-    #     except jwt.InvalidTokenError:
-    #         raise AuthenticationFailed("Invalid token.")
-    
-    # def _get_user_from_payload(self, payload):
-    #     username = payload.get('username')
-    #     if not username:
-    #         return None
-    #     try:
-    #         user = UserProfile.objects.get(username=username)
-    #     except UserProfile.DoesNotExist:
-    #         raise AuthenticationFailed("The user associated with this token does not exist.")
-    #     return user
     def authenticate(self, request):
-        authHeader = request.headers.get('Authorization')
-
-        if not authHeader:
+        auth_header = request.headers.get('Authorization')
+        if not auth_header:
             return None
-
-        try :
-            tokenType, token = authHeader.split()
-            if tokenType != 'Bearer':
-                message = "Bad Bearer"
-                raise AuthenticationFailed(message)
-        except ValueError :
-            message = "Auth Failed"
-            raise AuthenticationFailed(message)
-
-        try :
-            clear_token = jwt.decode(
-                    token,
-                    settings.SIMPLE_JWT['VERIFYING_KEY'],
-                    settings.SIMPLE_JWT['ALGORITHM'] 
-                    )
-        except jwt.ExpiredSignatureError :
-            message = "token expired"
-            raise AuthenticationFailed(message)
-        except jwt.InvalidTokenError:
-            message = "header info error"
-            raise AuthenticationFailed(message)
-        user = clear_token.get('username')
-        if user is None:
-            return(None, None)
+        token_type, token = self._extract_token(auth_header)
+        payload = self._decode_token(token)
+        user = self._get_user_from_payload(payload)
+        if user == None:
+            return (None, None)
+        return (user, token)
+    
+    def _extract_token(self, auth_header):
         try:
-            user_obj = UserProfile.objects.get(username=user)
+            token_type, token = auth_header.split()
+            if token_type != 'Bearer':
+                raise AuthenticationFailed("Invalid token type. Expected 'Bearer'.")
+            return token_type, token
+        except ValueError:
+            raise AuthenticationFailed("Invalid Authorization header format. Expected 'Bearer <token>'.")
+    
+    def _decode_token(self, token):
+        try:
+            return jwt.decode(
+                token,
+                settings.SIMPLE_JWT['VERIFYING_KEY'],
+                algorithms=settings.SIMPLE_JWT['ALGORITHM']
+            )
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("The token has expired.")
+        except jwt.InvalidTokenError:
+            raise AuthenticationFailed("Invalid token.")
+    
+    def _get_user_from_payload(self, payload):
+        username = payload.get('username')
+        if not username:
+            return None
+        try:
+            user = UserProfile.objects.get(username=username)
         except UserProfile.DoesNotExist:
-            message = "usr does nto exist"
-            raise AuthenticationFailed(message)
-        return(user_obj, token)
+            raise AuthenticationFailed("The user associated with this token does not exist.")
+        return user
