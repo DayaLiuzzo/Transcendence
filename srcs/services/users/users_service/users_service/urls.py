f622d5f4f7927e67c284e96a7f3f56351e2bcc58
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-02-10 18:17:04 +0100

    feat(Users) tests setup

--- srcs/services/users/users_service/users_service/urls.py
+++ srcs/services/users/users_service/users_service/urls.py
@@ -7,12 +7,8 @@ from users_app import views
 urlpatterns = [
     path('api/users/', include([
         path('create/', views.CreateUserProfileView.as_view(), name='create_user_profile'),
-        path('get/', views.get_example, name='get_example'),
         path('delete/<str:username>/', views.DeleteUserProfileView.as_view(), name='delete_user_profile'),
         path('update/<str:username>/', views.UpdateUserProfileView.as_view(), name='update_user_profile'),
-        path('test', views.TestServiceCommunicationView.as_view(), name='test'),  
-        path('protected/service/', views.ProtectedServiceView.as_view(), name='protected_view'),
-        path('protected/user/', views.ProtectedUserView.as_view(), name='protected_view'),
         path('<str:username>/', views.RetrieveUserProfile.as_view(), name='user_profile'),
         path('<str:username>/friends/add/<str:friendusername>/', views.AddFriendView.as_view(), name='add_friend'),
         path('<str:username>/friends/remove/<str:friendusername>/', views.RemoveFriendView.as_view(), name='remove_friend'),
