78d439cee944aaf44403cb1221bbd9b3a0cefa15
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-02-18 20:43:59 +0100

    feat(Frontend) add game test + Signup improvements

---
+++ srcs/services/frontend/public/src/views/Game.js
@@ -0,0 +1,33 @@
+import BaseView from './BaseView.js';
+
+
+export default class Game extends BaseView{
+    constructor(params){
+        super(params);
+    }
+
+    showError(message){
+        alert(message);
+    }
+
+    async fetchRoomInfo(){
+        const response = await fetch('https://localhost:4430/api/rooms/test/')
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+    }
+
+    async render(){
+        return `
+        <div>
+            <h2>Game</h2>
+            <h3> please just be ok</h3>
+        </div>
+    `;
+    }
+
+    async attachEvents(){
+        this.fetchRoomInfo();
+        console.log('Events attached (LogIn)');
+    }
+}
