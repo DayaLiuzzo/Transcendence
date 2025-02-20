4ab80c37c531520964bb71c8096d7b0701a06e83
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-02-20 14:32:25 +0100

    Feat(frontend) ADD BaseView.js SendPostRequest(url, formData) fully operational, LogIn.js semi operational (2fa left)

--- srcs/services/frontend/public/src/views/BaseView.js
+++ srcs/services/frontend/public/src/views/BaseView.js
@@ -8,6 +8,7 @@ export default class BaseView{
         this.API_URL = 'https://localhost:4430/api/auth/';
         this.API_URL_SIGNUP = 'https://localhost:4430/api/auth/signup/';
         this.API_URL_ROOMS = 'https://localhost:4430/api/rooms/';
+        this.API_URL_LOGIN = 'https://localhost:4430/api/auth/token/';
         this.app = document.getElementById('app');
         if (!this.app) {
             console.error("Error: Element with id 'app' not found in document");
@@ -29,8 +30,29 @@ export default class BaseView{
         this.router.navigateTo(path);
     }
     
+    async sendPostRequest(url, formData){
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            const responseData = await response.json();
+            if (!response.ok) {
+                console.error("Error in sendPostRequest():", url)
+                return { success: false, error: responseData};
+            }
+            return { success: true, data: responseData};
+        } 
+        catch (error) {
+            console.error("Network Error at ", url);
+            return { success: false, error: { message: "Network error"}};
+        }
+    }
+
     attachEvents(){
         console.log('Events attached');
     }
-
 }
