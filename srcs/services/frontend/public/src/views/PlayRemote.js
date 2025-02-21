b7d140abefea414d389534fdd78a860bd3295c8c
Marine <madavid@student.42.fr>
2025-02-21 16:45:05 +0100

    feat(Front for Game): tried fetch a post request in PlayRemote.js

--- srcs/services/frontend/public/src/views/PlayRemote.js
+++ srcs/services/frontend/public/src/views/PlayRemote.js
@@ -9,6 +9,14 @@ export default class PlayRemote extends BaseView{
         alert(message);
     }
 
+    async handleJoinRoom() {
+        const result = await this.sendPostRequest(this.API_URL + 'join_room/', {}); // Envoie une requête POST vide
+        if (result.success) {
+            document.getElementById("response-result").innerText = "Success: " + JSON.stringify(result.data, null, 2);
+        } else {
+            document.getElementById("response-result").innerText = "Error: " + JSON.stringify(result.error, null, 2);
+        }
+    }
 
     async render(){
         return `
@@ -16,15 +24,23 @@ export default class PlayRemote extends BaseView{
             <h2>Play remote</h2>
             
             <ol>
-                <li>Integrer les call api</li>
+                <li>Pop-up loader "Looking for playroom</li>
+                <li>Integrer les call api pour trouver un room</li>
                 <li>Integrer les websockets</li>
                 <li>Intgrer la 3d</li>
-
             </ol>
-           
+            <div id="response-result"></div>      
         </div>
     `;
     }
 
+    async mount() {
+        try {
+            this.app.innerHTML = await this.render();
+            await this.handleJoinRoom();
+        } catch (error) {
+            console.error("Error in mount():", error);
+        }
+    }
 
 }
