710d2d8b1b2cd4adb7a9c01ef02eb727cb270c56
Marine <madavid@student.42.fr>
2025-02-27 17:40:36 +0100

    fix(API_Gateway): allowing * to access api_gateway to allowed remote part

--- srcs/services/api_gateway/api_gateway_service/api_gateway_service/settings.py
+++ srcs/services/api_gateway/api_gateway_service/api_gateway_service/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("API_GATEWAY_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '*']
 
 
 # Application definition
@@ -126,10 +126,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://localhost:4430",  # SPA frontend URL
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://0.0.0.0:4430",  # SPA frontend URL
+# ]
 CORS_ALLOW_CREDENTIALS = True
 
 # CSRF_TRUSTED_ORIGINS = [
