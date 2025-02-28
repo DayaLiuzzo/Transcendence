5d71d150aca5e0710174d993dc2f3a565c6eb9c1
Marine <madavid@student.42.fr>
2025-02-28 15:39:17 +0100

    feat(Game): websocket checks if user is allowed in the room and code is clearer

--- srcs/services/game/game_service/game_app/consumers.py
+++ srcs/services/game/game_service/game_app/consumers.py
@@ -10,112 +10,120 @@ from django.shortcuts import get_object_or_404
 
 class GameConsumer(AsyncWebsocketConsumer):
     
-    @sync_to_async
-    def room_exists(self) -> bool:
-        from game_app.models import Game
-        #dans try and catch non ? changer la logique...
-        game = get_object_or_404(Game, room_id=self.room_name)
-        self.game = game
-        return Game.objects.filter(room_id=self.room_name).exists()
-
-    @sync_to_async
-    def room_is_full(self) -> bool:
-        return self.game.status=='playing'
-
-    @sync_to_async
-    def user_is_allowed(self) -> bool:
-        return self.game.player1.
-
-    player1 = models.ForeignKey('UserProfile', related_name='game_player1', null=True, blank=True, on_delete=models.SET_NULL)
-    player2 = models.ForeignKey('UserProfile', related_name='game_player2', null=True, blank=True, on_delete=models.SET_NULL)
-    
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'room%s' % self.room_name
         self.authenticated = False
         self.user = None
         self.game = None
-
         await self.accept()
         
     async def receive(self, text_data):
-        from .authentication import CustomJWTAuth
-        
         if not self.authenticated:
-            if not text_data:
-                await self.close()
-                return
-            
-            try:
-                text_data_json = json.loads(text_data)
-                print("Donnees recues:", text_data_json)
-            except json.JSONDecodeError:
-                await self.send_error_message("Invalid JSON format.")
-                await self.close()
-                return
-            message_data = text_data_json.get('message', {})
-            authorization_header = message_data.get('Authorization', '')
-            print("authorization_header :", authorization_header)
-            if authorization_header:
-                try:
-                    token_type, token = authorization_header.split(" ")
-                    if token_type != 'Bearer':
-                        raise AuthenticationFailed("Invalid token type. Expected 'Bearer'.")
-                except ValueError:
-                    await self.send_error_message("Invalid token format.")
-                    return
-
-                try:
-                    user = await CustomJWTAuth().authenticate_from_token(token)
-                except AuthenticationFailed as e:
-                    await self.send_error_message("User not allowed.")
-                    await self.close()
-                    return
-
-                self.user = user
-                self.authenticated = True
-
-                if not await self.room_exists():
-                    await self.send_error_message("Invalid room.")
-                    await self.close()
-                    return
-
-                if not await self.user_is_allowed():
-
-                await self.channel_layer.group_add(
-                    self.room_group_name,
-                    self.channel_name
-                )
-
-                isfull = await self.room_is_full(self.room_name)
-                await self.channel_layer.group_send(
-                    self.room_group_name,
-                    {
-                        'type': 'send_message',
-                        'message': f'{self.user.username} has joined the room.',
-                        'isfull': isfull,
-                    }
-                )   
-            else:
-                await self.send_error_message("Authorization header missing.")
-                await self.close()
+            await self.authenticate_user(text_data)
         else:
-            message = text_data_json.get('message', '')
-            if message:
-                await self.channel_layer.group_send(
-                    self.room_group_name,
-                    {
-                        'type': 'send_message',
-                        'message': message,
+            await self.handle_message(text_data)
 
-                    }
-                )
+    async def authenticate_user(self, text_data):
+        if not text_data:
+            await self.close()
+            return
+        
+        try:
+            text_data_json = json.loads(text_data)
+        except json.JSONDecodeError:
+            await self.send_error_message("Invalid JSON format.")
+            await self.close()
+            return
+        
+        message_data = text_data_json.get('message', {})
+        authorization_header = message_data.get('Authorization', '')
+        if not authorization_header:
+            await self.send_error_message("Authorization header missing.")
+            await self.close()
+            return
 
+        user = await self.authenticate_with_token(authorization_header)
+        if user is None:
+            return      
+
+        self.user = user
+
+        if not await self.room_exists():
+            await self.send_error_message("Invalid room.")
+            await self.close()
+            return
+
+        if not await self.user_is_allowed():
+            await self.send_error_message("User not allowed in room.")
+            await self.close()
+            return
+        
+        await self.channel_layer.group_add(self.room_group_name,self.channel_name)
+        self.authenticated = True
+        isfull = await self.room_is_full()
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'send_message',
+                'message': f'{self.user.username} has joined the room.',
+                'isfull': isfull,
+            }
+        )   
+                
+    async def authenticate_with_token(self, authorization_header):
+        try:
+            token_type, token = authorization_header.split(" ")
+            if token_type != 'Bearer':
+                await self.send_error_message("Invalid token type. Expected 'Bearer'.")
+        except ValueError:
+            await self.send_error_message("Invalid token format.")
+            return None
+
+        try:
+            from .authentication import CustomJWTAuth
+            user = await CustomJWTAuth().authenticate_from_token(token)
+        except AuthenticationFailed as e:
+            await self.send_error_message("User not allowed.")
+            await self.close()
+            return None
+        return user
+
+    async def handle_message(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json.get('message', '')
+        if message:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'send_message',
+                    'message': message,
+                }
+            )
+
+    @sync_to_async
+    def room_exists(self) -> bool:
+        from game_app.models import Game
+        try:
+            game = get_object_or_404(Game, room_id=self.room_name)
+            self.game = game
+            return True
+        except:
+            return False
+        
+    @sync_to_async
+    def room_is_full(self) -> bool:
+        return self.game.status=='playing'
+
+    @sync_to_async
+    def user_is_allowed(self) -> bool:
+        is_allowed = self.user == self.game.player1 or self.user == self.game.player2
+        return is_allowed
+    
     async def send_message(self, event):
         message = event['message']
         isfull = event['isfull']
         await self.send(text_data=json.dumps({'message': message, 'isfull': isfull}))
-        # await self.send(text_data=json.dumps(event))
 
     async def send_error_message(self, error_message):
         await self.send(text_data=json.dumps({'error': error_message}))
@@ -125,4 +133,5 @@ class GameConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
 pass
