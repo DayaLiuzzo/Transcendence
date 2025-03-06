c7c12ed601382f681b890ccd752d4df2fa54f856
Elise MALEUVRE <elmaleuv@paul-f4Br5s2.clusters.42paris.fr>
2025-03-06 18:11:19 +0100

    UPDATE: view PlayLocal ok, working on game dynamics

--- srcs/services/frontend/public/src/views/PlayLocal.js
+++ srcs/services/frontend/public/src/views/PlayLocal.js
@@ -1,161 +1,225 @@
-import BasePlayView from './BasePlayView.js';
+import BaseView from "./BaseView.js";
 
-export default class PlayLocal extends BasePlayView{
-    constructor(params){
-        super(params);
-    }
+let keys = { a: false, d: false, ArrowLeft: false, ArrowRight: false };
 
-    showError(message){
-        alert(message);
-    }
+export default class PlayCanva extends BaseView {
+	constructor(params) {
+		super(params);
+	}
 
-    // initGame(){
-    //     console.log("Game Loading...");
+	handlerEventsListeners() {
+		const cursor = { x: 0, y: 0 };
 
-    //     // ON SELECTIONNE LES ELEMENTS HTML NECESSAIRES POUR METTRE EN PLACE NOTRE SCENE
-    //     const canvas = document.querySelector(".webgl");
-    //     const container = document.getElementById("container-canvas-game-local");
-    //     const asciiOutput = document.getElementById("ascii-effect-output");
+		window.addEventListener("mousemove", (event) => {
+			cursor.x = event.clientX / window.innerWidth - 0.5;
+			cursor.y = -(event.clientY / window.innerHeight - 0.5);
+		});
 
-    //     // CREATION DE LA SCENE THREEJS && CHARGER LES MODELES 3D GLTF VIA LE LOADER DE THREEJS
-    //     const scene = new THREE.Scene();
-    //     // const gltfLoader = new THREE.GLTFLoader();
-    //     // console.log(gltfLoader);
+		window.addEventListener("keydown", (event) => {
+			if (event.key === "ArrowLeft") {
+				keys.ArrowLeft = true;
+				console.log(keys.ArrowLeft);
+				//this.displayPaddle(meshPaddleLeft, deltaTime, -1);
+			}
+			if (event.key === "ArrowRight") {
+				keys.ArrowRight = true;
+				console.log(keys.ArrowRight);
+				console.log(keys);
+				//this.displayPaddle(meshPaddleLeft, deltaTime, 1);
+			}
+			if (event.key === "a") {
+				keys.a = true;
+				console.log(keys.a);
+				//this.displayPaddle(meshPaddleRight, deltaTime, -1);
+			}
+			if (event.key === "d") {
+				keys.d = true;
+				console.log(keys.d);
+				//	this.displayPaddle(meshPaddleRight, deltaTime, 1);
+			}
+		});
 
-    //     // gltfLoader.load(
-    //     //     "../../assets/models/",
-    //     //     (gltf) => {
-    //     //         console.log(gltf);
-    //     //         scene.add(gltf.scene);
-    //     //     }
-    //     // );
+		window.addEventListener('keyup', (event) => {
+			if (event.key === 'ArrowLeft') {
+				keys.ArrowLeft = false;
+				console.log(keys.ArrowLeft);
+			}
+			if ( event.key === 'ArrowRight') {
+				keys.ArrowRight = false;
+				console.log(keys.ArrowRight);
+				console.log(keys);
+			}
+			if (event.key === 'a') {
+				keys.a = false;
+				console.log(keys.a);
+			}
+			if ( event.key === 'd') {
+				keys.d = false;
+				console.log(keys.d);
+			}
+		});
+	}
 
-    //     const meshPaddleLeft = new THREE.Mesh(
-    //         new THREE.BoxGeometry(30, 1, 1),
-    //         new THREE.MeshBasicMaterial({ color: 0xff0000 })
-    //     );
+	displayPaddle(meshPaddleLeft, meshPaddleRight, deltaTime) {
 
-    //     const meshPaddleRight = new THREE.Mesh(
-    //         new THREE.BoxGeometry(30, 1, 1),
-    //         new THREE.MeshBasicMaterial({ color: 0x0000ff })
-    //     );
+		const paddleSpeed = 5;
 
-    //     const meshBall = new THREE.Mesh(
-    //         new THREE.SphereGeometry(0.5, 32, 32),
-    //         new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    //     );
+		if (keys.ArrowLeft ) {
+			meshPaddleLeft.position.x -= paddleSpeed * deltaTime;
+			console.log(meshPaddleLeft.position.x);
+		}
+		if (keys.ArrowRight ) {
+			meshPaddleLeft.position.x += paddleSpeed * deltaTime;
+		}
+		if (keys.a) {
+			meshPaddleRight.position.x -= paddleSpeed * deltaTime;
+		}
+		if (keys.d) {
+			meshPaddleRight.position.x += paddleSpeed * deltaTime;
+		}
+	}
 
-    //     const sizes = {
-    //         width: container.innerWidth,
-    //         height: container.innerHeight
-    //     };
+	// updateBallPosition(meshBall, deltaTime) {
+	// 	const ballSpeed = 0.1;
+	// 	document.addEventListener('keydown', (event) => {
+	// 		if (event.key == 'ArrowUp') {
+	// 			meshBall.position.y += ballDirection.y * (ballSpeed * deltaTime);
+	// 			console.log(meshBall.position.y);
+	// 		}
+	// 		if (event.key == 'ArrowDown') {
+	// 			meshBall.position.y -= ballDirection.y * (ballSpeed * deltaTime);
+	// 		}
+	// });
 
-    //     scene.add(meshPaddleLeft);
-    //     scene.add(meshPaddleRight);
-    //     scene.add(meshBall);
+	// }
 
-    //     const light = new THREE.DirectionalLight(0xffffff, 1);
-    //     light.position.set(2, 2, 2);
-    //     scene.add(light);
+	displayBall(meshBall, positionXDuBack, positionYDuBack, deltaTime) {
+		meshBall.position.y = positionYDuBack * deltaTime;
+		meshBall.position.x = positionXDuBack * deltaTime;
+	}
 
-    //     const renderer = new THREE.WebGLRenderer({
-	// 		canvas: canvas,
-	// 		alpha: true,
-	// 	});
-	// 	renderer.setSize(sizes.width, sizes.height);
-	// 	renderer.setPixelRatio(window.devicePixelRatio);
+	initGame() {
+		console.log("Game Loading...");
 
-    //     const asciiChar = " .:-+*=%@";
-	// 	const effect = new THREE.AsciiEffect(renderer, asciiChar, {
-	// 		invert: true,
-	// 		resolution: 0.25,
-	// 		scale: 1,
-	// 	});
-    //     effect.setSize(sizes.width, sizes.height);
-	// 	effect.domElement.style.color = "black";
-	// 	effect.domElement.style.backgroundColor = "none";
+		const canvas = document.querySelector("canvas.webgl");
+		//	const container = document.getElementById("container-canvas-game-canva");
+		//	const asciiOutput = document.getElementById("ascii-output");
 
-	// 	// on cache le rendu de base pour laisser apparaitre le rendu ascii
-	// 	effect.domElement.classList.add("ascii-effect");
-	// 	effect.domElement.style.cursor = "grab";
-	// 	document.querySelector("#ascii-output").appendChild(effect.domElement);
-	// 	canvas.style.display = "none";
-	// 	// camera perspective etc
-	// 	const camera = new THREE.PerspectiveCamera(
-	// 		75,
-	// 		sizes.width / sizes.height
-	// 	);
-	// 	camera.position.z = 1;
-	// 	camera.position.y = 1;
-	// 	camera.lookAt(0, 0, 0);
-	// 	//camera.lookAt(mesh.position);
-	// 	scene.add(camera);
+		const scene = new THREE.Scene();
 
-	// 	// controls for the camera
-	// 	const controls = new THREE.OrbitControls(camera, effect.domElement);
-	// 	controls.enableDamping = true;
-	// 	controls.enableZoom = true;
-	// 	renderer.domElement.style.cursor = "grab";
+		const meshPaddleLeft = new THREE.Mesh(
+			new THREE.BoxGeometry(40, 2, 1),
+			new THREE.MeshBasicMaterial({ color: 0x000000 })
+		);
+		meshPaddleLeft.position.x = 3;
+		meshPaddleLeft.position.y = -10;
 
-	// 	window.threeInstance = {
-	// 		scene,
-	// 		camera,
-	// 		renderer,
-	// 		effect,
-	// 		controls,
-	// 		animationId: null,
-	// 		canvas,
-	// 		container,
-	// 	};
+		const meshPaddleRight = new THREE.Mesh(
+			new THREE.BoxGeometry(40, 2, 1),
+			new THREE.MeshBasicMaterial({ color: 0x000000 })
+		);
+		meshPaddleRight.position.x = 3;
+		meshPaddleRight.position.y = 1.5;
 
-	// 	const clock = new THREE.Clock();
+		const meshBall = new THREE.Mesh(
+			new THREE.SphereGeometry(0.5, 32, 32),
+			new THREE.MeshBasicMaterial({ color: 0x000000 })
+		);
+		meshBall.position.y = 0;
 
-	// 	const tick = () => {
-	// 		const elapsedTime = clock.getElapsedTime();
-	// 		//mesh.rotation.y = elapsedTime * 0.5;
+		const sizes = {
+			width: window.innerWidth,
+			height: window.innerHeight,
+		};
+		console.log(sizes);
 
-	// 		controls.update();
-	// 		window.threeInstance.effect.render(scene, camera);
-	// 		window.threeInstance.animationId = requestAnimationFrame(tick);
-	// 	};
+		scene.add(meshPaddleLeft);
+		scene.add(meshPaddleRight);
+		scene.add(meshBall);
 
-	// 	window.addEventListener("resize", () => {
-	// 		const rect = container.getBoundingClientRect();
+		const light = new THREE.DirectionalLight(0xffffff, 1);
+		light.position.set(2, 2, 2);
+		scene.add(light);
 
-	// 		sizes.width = rect.width;
-	// 		sizes.height = rect.height;
+		const renderer = new THREE.WebGLRenderer({
+			canvas: canvas,
+			alpha: true,
+		});
 
-	// 		camera.aspect = sizes.width / sizes.height;
-	// 		camera.updateProjectionMatrix();
+		const asciiChar = " .";
+		const effect = new THREE.AsciiEffect(renderer, asciiChar, {
+			invert: false,
+			resolution: 0.2,
+			scale: 1,
+		});
+		effect.setSize(sizes.width, sizes.height);
+		effect.domElement.style.color = "black";
+		effect.domElement.style.backgroundColor = "none";
 
-	// 		window.threeInstance.renderer.setSize(sizes.width, sizes.height);
-	// 		window.threeInstance.effect.setSize(sizes.width, sizes.height);
-	// 		window.threeInstance.renderer.setPixelRatio(
-	// 			Math.min(window.devicePixelRatio, 2)
-	// 		);
-	// 	});
+		//effect.domElement.classList.add("ascii-effect");
+		//effect.domElement.style.cursor = "grab";
+		//document.querySelector("#ascii-output").appendChild(effect.domElement);
+		//canvas.style.display = "none";
 
-	// 	tick();
+		const camera = new THREE.PerspectiveCamera(
+			75,
+			sizes.width / sizes.height
+		);
+		camera.position.z = 20;
 
-    // }
+		//camera.position.y = 1;
+		//camera.lookAt(meshPaddleLeft.position);
+		//camera.lookAt(mesh.position);
+		scene.add(camera);
 
+		renderer.setSize(sizes.width, sizes.height);
+		renderer.setPixelRatio(window.devicePixelRatio);
 
-    render(){
-        return `
+		//const controls = new THREE.OrbitControls(camera, effect.domElement);
+		//renderer.domElement.style.cursor = "grab";
+		const controls = new THREE.OrbitControls(camera, canvas);
+		console.log(controls);
+		controls.enableDamping = true;
+		controls.enableZoom = true;
+
+		const clock = new THREE.Clock();
+		const tick = () => {
+			let deltaTime = clock.getDelta();
+			//const elapsedTime = clock.getElapsedTime();
+			//meshPaddleLeft.rotation.y = elapsedTime * 0.5;
+			//meshPaddleRight.rotation.y = elapsedTime * 0.5;
+			//meshBall.rotation.y = elapsedTime * 0.5;
+			//meshBall.position.x = Math.sin(elapsedTime);
+
+			this.displayPaddle(meshPaddleLeft, meshPaddleRight, deltaTime);
+
+			controls.update();
+			renderer.render(scene, camera);
+			//effect.render(scene, camera);
+			//effect.animationId = requestAnimationFrame(tick);
+			//window.threeInstance.animationId = requestAnimationFrame(tick);
+			requestAnimationFrame(tick);
+		};
+
+		tick();
+	}
+
+	render() {
+		return `
         <div id="line"></div>
 
-        <div id="container-canvas-game-local">
+        <div id="container-canvas-game-canva">
             <canvas class="webgl"></canvas>
-            <div id="ascii-effect-output"></div>
+            <div id="ascii-output"></div>
         </div>
             <div id="response-result"></div>
         </div>
     `;
-    }
+	}
 
-    attachEvents(){
-        console.log("Events attached (PlayLocal)");
-     // this.initGame();
-    }
-
+	attachEvents() {
+		console.log("Events attached (PlayCanva)");
+		this.handlerEventsListeners();
+		this.initGame();
+	}
 }
