3dee8d2d60b71b2383fb5a5535a7468245afed89
K-Pascal <152603632+K-Pascal@users.noreply.github.com>
2025-03-07 20:57:40 +0100

    chore: add docker watch on game service

--- srcs/services/game/init/start.sh
+++ srcs/services/game/init/start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-pip install --upgrade pip --no-input
-pip install -r /init/requirements.txt --no-input
-pip install channels
-pip install uvicorn[standard]
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
