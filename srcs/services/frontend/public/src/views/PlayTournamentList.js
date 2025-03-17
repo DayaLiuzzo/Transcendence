9c1fcce3c89e74c74aa7152a82d5a6500fac654f
Marine <madavid@student.42.fr>
2025-03-17 19:25:35 +0100

    feat(frontend x tournament): list tournament page to list and join tournaments

--- srcs/services/frontend/public/src/views/PlayTournamentList.js
+++ srcs/services/frontend/public/src/views/PlayTournamentList.js
@@ -4,18 +4,81 @@ import BaseView from './BaseView.js';
 
 export default class PlayTournamentList extends BaseView{
     
-    constructor(params){
-        super(params);
+    constructor(router, params) {
+        super(router, params);
     }
-    
-    render(){
+
+
+    render() {
         return `
         <div>
-            <h2>Play Tournament List (Pas pret !!!)</h2>
-            <p>afficher une list<\p>
-            
+            <h2>List tournament</h2>
+
+            <div id="tournament-list-field"></div>
         </div>
-    `;
+        `;
+    }
+
+
+
+    attachEvents() {
+        console.log('Events attached (Tournament list)');
+
+
+        const tournamentListField = document.getElementById("tournament-list-field");
+        // if (tournamentListField) {
+        //     tournamentListField.addEventListener("click", this.handleFriendRemoveClick.bind(this));
+        // } on va l'utiliser pour join
+
+    }
+
+    renderTournamentList(tournaments) {
+        console.log("Coucou!!!!!!!!!")
+        const tournamentListField = document.getElementById("tournament-list-field");
+        if (!tournamentListField) return;
+        console.log("Toujours la!!!!!!!!!")
+        tournamentListField.innerHTML = "";
+        const tournamentList = document.createElement("ul");
+        tournaments.forEach(tournament => {
+            const tournamentItem = document.createElement("li");
+            console.log(tournament.name);
+            tournamentItem.textContent = tournament.name;
+
+            const joinButton = document.createElement("button");
+            joinButton.textContent = "Join";
+            joinButton.setAttribute("data-tournamentname", tournament.name);
+            tournamentItem.appendChild(joinButton);
+            tournamentList.appendChild(tournamentItem);
+        });
+        tournamentListField.appendChild(tournamentList);
+    }
+
+    async mount() {
+        try {
+            const getTournamentList = await this.sendGetRequest(this.API_URL_TOURNAMENT + '/list/');
+            if (!getTournamentList.success){
+                console.log("Erreur fetch get tournament list")
+                return this.showError(getTournamentList.error)
+            }
+            console.log("Success fetch get tournament list")
+            console.log(getTournamentList.data)
+            
+            const tournaments = Array.isArray(getTournamentList.data) ? getTournamentList.data : [getTournamentList.data];
+            console.log("tournament --> ", tournaments)
+            this.renderTournamentList(tournaments);
+        }
+        catch (error) {
+            console.error("Error in mount():", error);
+        }
+    }
+
+    unmount() {
+        console.log('Unmounting Profile');
+        
+        const tournamentListField = document.getElementById("tournament-list-field");
+        if (tournamentListField) {
+            tournamentListField.removeEventListener("click", this.handleRemoveFriendClick);
+        }
     }
 
 }
