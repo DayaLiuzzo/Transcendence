184e5c3081e3de4fec09413b0e3fa3e73b064eb2
Marine <madavid@student.42.fr>
2025-03-18 17:33:41 +0100

    feat(frontend x tournament): add form in join tournament page to enter specific tournament id to join (need to check if it works with a right id, not done yet)

--- srcs/services/frontend/public/src/views/PlayTournamentJoin.js
+++ srcs/services/frontend/public/src/views/PlayTournamentJoin.js
@@ -6,17 +6,56 @@ export default class PlayTournamentJoin extends BaseView{
     
     constructor(params){
         super(params);
+        this.handleJoinTournamentSubmit = this.handleJoinTournamentSubmit.bind(this);
+
     }
     
+    //inputs a valider ou pas ?
+
+
+    handleJoinTournamentSubmit(event){
+        event.preventDefault();
+        const formData = this.getFormData();
+        this.joinTournament(formData.id);
+    }
+
+    async joinTournament(tournamentId) {
+        const body = {};
+
+        const response = await this.sendPostRequest(this.API_URL_TOURNAMENT + "join/" + tournamentId + "/", body);
+        if (!response.success) { return this.showError(response.error, "tournament-join-field"); }
+
+        this.navigateTo("/my-tournament");
+    }
+    
+
+    getFormData(){
+        return {
+            id: document.getElementById("joinTournament-id").value,
+        };
+    }
 
     render(){
         return `
         <div>
             <h2>Join Tournament (Pas pret !!!!)</h2>
-            <p>afficher un form ou bien une liste<\p>
-            
+            <form id="joinTournament-form">
+                <input type="text" id="joinTournament-id" placeholder="Tournament id" required>
+                <button type="submit">Join tournament</button>
+            </form>
+            <div id="tournament-join-field"></div>
         </div>
     `;
+    }
+
+    unmount(){
+        console.log('unmounting join tournament');
+        document.getElementById("joinTournament-form")?.removeEventListener("submit", this.handleJoinTournamentSubmit);
+    }
+
+    attachEvents(){
+        console.log('Events attached (join tournament)');
+        document.getElementById("joinTournament-form")?.addEventListener("submit", this.handleJoinTournamentSubmit);
     }
 
 }
