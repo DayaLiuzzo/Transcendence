6150be4a4548c0976e5191a550160b16596735aa
K-Pascal <152603632+K-Pascal@users.noreply.github.com>
2025-04-07 19:53:28 +0200

    refact: add auth to docker watch files

--- srcs/services/auth/init/start.sh
+++ srcs/services/auth/init/start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-
-openssl genpkey -algorithm RSA -out /private_key/sjwt_private.pem -pkeyopt rsa_keygen_bits:2048
-openssl rsa -pubout -in /private_key/sjwt_private.pem -out /keys/sjwt_public.pem
-
-
-pip install --upgrade pip --no-input
-pip install -r /init/requirements.txt --no-input
 python3 manage.py makemigrations --no-input
 python3 manage.py migrate --no-input
 python3 manage.py generate_service_tokens
